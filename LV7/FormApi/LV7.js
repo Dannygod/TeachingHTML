be40a90bd3e71c6c2e2d12befebb222920a0f0dc
Danny Hsu <danny539425@gmail.com>
2023-12-15 23:20:11 +0800

    12/15

--- LV7/FormApi/LV7.js
+++ LV7/FormApi/LV7.js
@@ -1,14 +1,19 @@
 let form = document.getElementById('sheetdb-form');
 form.addEventListener("submit", e => {
     e.preventDefault();
+    let password = document.getElementById('PW').value;
+    if(password.trim() === ''){
+        alert('密碼不能為空!');
+        return;
+    }
     fetch(form.action, {
         method : "POST",
-        body: new FormData(document.getElementById("sheetdb-form")),
+        body: new FormData(form),
     }).then(
         response => response.json()
     ).then((html) => {
         // you can put any JS code here
-        window.open('page2.html', '_blank');
+        window.open('page2.html', '_self');
         console.log("success")
     });
 });
