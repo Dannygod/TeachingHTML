2faf270876141cd4e8223907edd00e697a3377b1
Danny Hsu <danny539425@gmail.com>
2023-12-15 15:57:37 +0800

    form

---
+++ LV7/FormApi/LV7.js
@@ -0,0 +1,14 @@
+let form = document.getElementById('sheetdb-form');
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    fetch(form.action, {
+        method : "POST",
+        body: new FormData(document.getElementById("sheetdb-form")),
+    }).then(
+        response => response.json()
+    ).then((html) => {
+        // you can put any JS code here
+        window.open('page2.html', '_blank');
+        console.log("success")
+    });
+});
